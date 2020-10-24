bbf2489bcd8a193aa9b590c044f1248b47a3f4e2
szk <szk@192.168.15.3>
2020-10-24 19:18:13 +0200

    branch1 elso

--- haromszog/src/haromszog/hprint.java
+++ haromszog/src/haromszog/hprint.java
@@ -6,10 +6,15 @@ public class hprint {
 	public void prnum() {
 		// TODO Auto-generated method stub
 		System.out.println("a10");
+		System.out.println("Branc1_10");
 		System.out.println("a20");
+		System.out.println("Branc1_10");
 		System.out.println("a30");
+		System.out.println("Branc1_20");
 		System.out.println("a40");
+		System.out.println("Branc1_30");
 		System.out.println("a50");
+		System.out.println("Branc1_40");
 		System.out.println("a60");
 	}
 	
