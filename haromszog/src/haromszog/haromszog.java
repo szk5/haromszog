fcef30d54dc6bfeb4e23c4926dfb9cd7652196df
szk <szk@192.168.15.3>
2020-10-24 18:36:46 +0200

    init

---
+++ haromszog/src/haromszog/haromszog.java
@@ -0,0 +1,11 @@
+package haromszog;
+
+public class haromszog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hprint hp = new hprint();
+		hp.prnum();
+	}
+
+}
---
+++ haromszog/src/haromszog/hprint.java
@@ -0,0 +1,16 @@
+package haromszog;
+
+public class hprint {
+
+	
+	public void prnum() {
+		// TODO Auto-generated method stub
+		System.out.println("a10");
+		System.out.println("a20");
+		System.out.println("a30");
+		System.out.println("a40");
+		System.out.println("a50");
+		System.out.println("a60");
+	}
+	
+}
